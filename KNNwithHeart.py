02bf9dd968033966d6f8cec9ed2763f8795c60b5
tessertitan1122 <tessertitan1122@gmail.com>
2025-04-09 14:24:53 +0700

    .

---
+++ KNNwithHeart.py
@@ -0,0 +1,95 @@
+from sklearn.neighbors import KneighborsClassifier
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+st.title('การจำแนกข้อมูลด้วยเทคนิค Machine Learnning')
+# st.image("./img/go1.jpg")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Versicolor")
+   st.image("./img/iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./img/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./img/iris3.jpg")
+
+html_7 = """
+<div style="background-color:#39edf6;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h4>ข้อมูล iris หรือข้อมูลดอกไม้สำหรับทำนาย</h5></center>
+</div>
+"""
+st.markdown(html_7, unsafe_allow_html=True)
+st.markdown("")
+st.markdown("")
+
+st.subheader("ข้อมูลส่วนแรก 10 แถว")
+dt = pd.read_csv("./data/iris-3.csv")
+st.write(dt.head(10))
+st.subheader("ข้อมูลส่วนสุดท้าย 10 แถว")
+st.write(dt.tail(10))
+
+# สถิติพื้นฐาน
+st.subheader("📈 สถิติพื้นฐานของข้อมูล")
+st.write(dt.describe())
+
+# การเลือกแสดงกราฟตามฟีเจอร์
+st.subheader("📌 เลือกฟีเจอร์เพื่อดูการกระจายข้อมูล")
+feature = st.selectbox("เลือกฟีเจอร์", dt.columns[:-1])
+
+# วาดกราฟ boxplot
+st.write(f"### 🎯 Boxplot: {feature} แยกตามชนิดของดอกไม้")
+fig, ax = plt.subplots()
+sns.boxplot(data=dt, x='variety', y=feature, ax=ax)
+st.pyplot(fig)
+
+# วาด pairplot
+if st.checkbox("แสดง Pairplot (ใช้เวลาประมวลผลเล็กน้อย)"):
+    st.write("### 🌺 Pairplot: การกระจายของข้อมูลทั้งหมด")
+    fig2 = sns.pairplot(dt, hue='variety')
+    st.pyplot(fig2)
+
+
+html_8 = """
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ทำนายข้อมูล</h5></center>
+</div>
+"""
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
+
+pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
+pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
+
+sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
+sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
+
+if st.button("ทำนายผล"):
+    #st.write("ทำนาย")
+   dt = pd.read_csv("./data/iris-3.csv") 
+   X = dt.drop('variety', axis=1)
+   y = dt.variety   
+
+   Knn_model = KNeighborsClassifier(n_neighbors=3)
+   Knn_model.fit(X, y)  
+    
+   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
+   st.write(Knn_model.predict(x_input))
+   
+   out=Knn_model.predict(x_input)
+
+   if out[0] == 'Setosa':
+    st.image("./img/iris1.jpg")
+   elif out[0] == 'Versicolor':       
+    st.image("./img/iris2.jpg")
+   else:
+    st.image("./img/iris3.jpg")
+else:
+    st.write("ไม่ทำนาย")
